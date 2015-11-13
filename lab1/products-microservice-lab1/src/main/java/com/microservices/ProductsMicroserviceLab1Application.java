efdeee56a56968703dec59e679a2a7cb8681b90e
rommel.oramas@gmail.com <rommel.oramas@gmail.com>
2015-11-13 14:31:08 -0500

    Proyecto products-microservice lab1

---
+++ lab1/products-microservice-lab1/src/main/java/com/microservices/ProductsMicroserviceLab1Application.java
@@ -0,0 +1,12 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsMicroserviceLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsMicroserviceLab1Application.class, args);
+    }
+}
