3eae035b0c3c17bee9ec687f0149830d42a44670
rommel.oramas@gmail.com <rommel.oramas@gmail.com>
2015-12-08 09:15:44 -0500

    Lab3 commit

---
+++ lab3/configuration-server-lab3/src/main/java/com/microservices/ConfigurationServerLab3Application.java
@@ -0,0 +1,16 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigurationServerLab3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServerLab3Application.class, args);
+    }
+}
